bc6a3d22a9bebe3f3db1141c177e32b6f37f20c0
Mazino Ukah <ewomaukah@yahoo.com>
2018-04-26 11:27:09 +0100

    Refactored store.js

--- src/renderer/store/modules/store.js
+++ src/renderer/store/modules/store.js
@@ -1,6 +1,5 @@
-import Vue from 'vue'
 import BaseModule from '../utils/BaseModule'
-// import axios from 'axios';
+import { storeSetup } from '../../service/endpoints'
 
 export default new BaseModule('sale', {
   state: {
@@ -8,34 +7,22 @@ export default new BaseModule('sale', {
   },
   actions: {
     setStoreDetails ({ commit }, payload) {
-      return Vue.axios
-        .post('forms/setup.php', payload, {
-          headers: { 'Content-Type': 'multipart/form-data' }
-        })
-        .then(res => {
-          // console.log(res.data);
-          // commit('SET_STORE_DETAILS', res.data);
-          return res.data
-        })
+      return storeSetup(payload).then(res => {
+        // commit("SET_STORE_DETAILS", res.data)
+        return res.data
+      })
     },
     getStoreDetails ({ commit }, payload) {
-      return Vue.axios
-        .post('forms/setup.php', payload, {
-          headers: { 'Content-Type': 'multipart/form-data' }
-        })
-        .then(res => {
-          // console.log(res.data);
-          // commit('SET_STORE_DETAILS', res.data);
-          return res.data
-        })
+      return storeSetup(payload).then(res => {
+        // commit('SET_STORE_DETAILS', res.data)
+        return res.data
+      })
     }
   },
   mutations: {
     SET_STORE_DETAILS (state, data) {
-      console.log(data.currency)
       state.store = {
         ...data,
-        // setuployaltysetting: 'setuployaltysetting',
         taxes: data.tax ? JSON.parse(data.tax) : [],
         currency: data.currency ? JSON.parse(data.currency) : ''
       }
